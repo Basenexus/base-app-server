3d82194c65c562c9408281ea2e787df692691822
Roman <roma.bruhanchik@gmail.com>
2023-06-15 01:20:09 +0400

    fix failing tests, add embedded test db

--- src/test/java/com/base/baseappserver/BaseAppServerApplicationTests.java
+++ src/test/java/com/base/baseappserver/BaseAppServerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.base.baseappserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BaseAppServerApplicationTests {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(applicationContext);
     }
 
 }
