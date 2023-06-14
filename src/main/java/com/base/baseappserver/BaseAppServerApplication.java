1b2995f7d900a630180927f29ea1ad18191f6ae5
Roman <roma.bruhanchik@gmail.com>
2023-06-14 23:49:46 +0400

    Initial commit

---
+++ src/main/java/com/base/baseappserver/BaseAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.base.baseappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/base/baseappserver/BaseAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.baseappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
